1ed89e5b4c9b6129d43f25c85cd4ee7783c776cd
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-09-02 02:09:30 -0400

    Update agregarcharms.lua

--- agregarcharms.lua
+++ agregarcharms.lua
@@ -1,12 +1,26 @@
 local action = Action()
 
 function action.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+    if not player or not item or item:getId() ~= 37317 then
+        return false
+    end
 
-    player:addCharmPoints(5) -- cantidad de charms a agregar
-    player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ganaste 5 Charms")
-    item:remove()
+    local charmAmountToAdd = 5
 
+    -- Verifica si el objeto es apilable y tiene más de 1 en la pila
+    if item:isStackable() and item:getCount() > 1 then
+        player:addCharmPoints(charmAmountToAdd)
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ganaste " .. charmAmountToAdd .. " Charms")
+        item:remove(1)  -- Elimina solo 1 objeto del montón
+    else
+        -- Si no es apilable o solo queda 1 en la pila, elimina todo el montón
+        player:addCharmPoints(charmAmountToAdd * item:getCount())
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ganaste " .. charmAmountToAdd * item:getCount() .. " Charms")
+        item:remove()
+    end
+
+    return true
 end
 
-action:id(37317) --- id del objeto que usaras para que funcione
+action:id(37317)
 action:register()
